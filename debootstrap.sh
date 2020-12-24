2a90f689dc328fb3436fdf1241f6499c1588ca86
Nikita Mokhnachev <61989697+fumanchouz@users.noreply.github.com>
2020-12-24 08:58:48 +0300

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -5,15 +5,12 @@ ARCH=amd6
 DISTRO=sid
 CHROOT=/mnt
 
-debootstrap                                                 \
-	--include=sudo, zsh, vim, wget, curl,                   \
-		linux-base, linux-image-$ARCH, linux-headers,       \
-		firmware-amd-graphics                               \
-		firmware-linux, firmware-misc-nonfree,              \
-		firmware-atheros, firmwate-ralink, firmware-realtek \
-	--arch $ARCH                                            \
-	$DISTRO                                                 \
-	$CHROOT                                                 \
+mkdir $CHROOT/{boot,dev,sys}
+
+debootstrap                            \
+	--include=sudo, zsh, vim       \
+	--arch $ARCH                   \
+	$DISTRO $CHROOT                \
 	http://mirror.yandex.ru/$OS
 
 mount -o bind /dev $CHROOT/dev
