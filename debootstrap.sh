bbda0dde6c7db7d57be86c56696a3f1361e3d49e
Nikita Mokhnachev <61989697+fumanchouz@users.noreply.github.com>
2020-12-24 10:20:55 +0300

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#! /bin/bash
 
 OS=debian
 ARCH=amd6
 DISTRO=sid
 CHROOT=/mnt
 
-mkdir $CHROOT/{boot,dev,sys}
+if [ -d $CHROOT/{boot,dev,sys} ]
+then
+  mkdir $CHROOT/{boot,dev,sys}
+fi
 
-debootstrap                            \
-	--include=sudo, zsh, vim       \
-	--arch $ARCH                   \
-	$DISTRO $CHROOT                \
-	http://mirror.yandex.ru/$OS
+debootstrap                      \
+  --include=sudo,vim,zsh         \
+  --arch $ARCH                   \
+  $DISTRO $CHROOT                \
+  http://mirror.yandex.ru/$OS
 
-mount -o bind /dev $CHROOT/dev
-mount -o bind /sys $CHROOT/sys
+for dir in dev sys proc
+do
+  mount -o bind $dir $CHROOT/$dir
+done
+
+chroot $CHROOT
